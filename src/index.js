4387813f740eb0423bb6ff5cdb5d26b1133d60f1
saurabh0413 <93505734+saurabh0413@users.noreply.github.com>
2022-01-18 02:39:01 +0530

    t

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const indexController = require("./controllers/index.controller")
 const app = express();
 app.use(express.json());
 
 app.set("view engine", "ejs"); 
 app.use(express.static("public"));
+
+app.use("", indexController);
+
 module.exports = app;
