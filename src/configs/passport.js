528aff80c06044093898c46f4e9e64af00d22d76
Durgesh Kumar Singh <93372022+Durgesh2601@users.noreply.github.com>
2022-01-20 12:49:56 +0530

    updated oauth

--- src/configs/passport.js
+++ src/configs/passport.js
@@ -5,7 +5,7 @@ const GoogleStrategy = require('passport-google-oauth2').Strategy;
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:2389/auth/google/callback",
+  callbackURL: "http://localhost:8767/auth/google/callback",
   passReqToCallback: true,
 },
 function(request, accessToken, refreshToken, profile, done) {
