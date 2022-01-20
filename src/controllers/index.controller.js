faaabcd3d3f2a2907e39635e6d546791e35e1b25
KeshavManuja <93030991+KeshavManuja@users.noreply.github.com>
2022-01-20 10:37:49 +0530

    Update

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -7,4 +7,28 @@ router.get("", (req, res) => {
   return res.render("index");
 });
 
+<<<<<<< Updated upstream
+=======
+router.get("/product", async(req, res) => {
+  try{
+    const product = await Product.find().lean().exec() 
+    // console.log(product)
+    return res.render("product",{product:product});
+  }
+  catch(err){
+    return res.status(500).send({err:err.message})
+  }
+});
+router.get("/unqProduct/:id", async(req, res) => {
+  try{
+    const uproduct = await Product.findById(req.params.id).lean().exec()
+    // console.log(product)
+    return res.render("unqProduct",{uproduct:uproduct});
+  }
+  catch(err){
+    return res.status(500).send({err:err.message})
+  }
+});                                                                     
+
+>>>>>>> Stashed changes
 module.exports = router
